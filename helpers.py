d90a122daddb2333736af7226b7ac4dd76fcd9ae
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-08 13:08:45 +0300

    Добавлен метод генерирования данных для создания курьера в helpers.py

---
+++ helpers.py
@@ -0,0 +1,27 @@
+import random
+from faker import Faker
+import random
+import string
+
+
+# Метод генерирует строку, состоящую только из букв нижнего регистра, в качестве параметра передаём длину строки
+def generate_random_string(length):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(length))
+
+
+# Метод генерирования данных для регистрации нового курьера
+def generate_new_courier_login_password_first_name():
+    # генерируем логин, пароль и имя курьера
+    login = generate_random_string(10)
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+    
+    payload = {
+        "login": login,
+        "password": password,
+        "firstName": first_name
+        }
+    
+    return payload
+
