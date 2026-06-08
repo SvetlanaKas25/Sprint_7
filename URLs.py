0c5dfafb6fb3d5ba9c7bf868d970bbaedd54248e
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-08 13:31:31 +0300

    Добавлена Ручка - Создание заказа в URLs.py

--- URLs.py
+++ URLs.py
@@ -1,3 +1,4 @@
+
 class URL:
     BASE_URL = "https://qa-scooter.praktikum-services.ru"
     
@@ -6,3 +7,7 @@ class URL:
 
     # Ручка - Логин курьера в системе
     LOGIN_COURIER_ENDPOINT = f"{BASE_URL}/api/v1/courier/login"
+    
+    # Ручка - Создание заказа
+    CREATE_ORDER_ENDPOINT = f"{BASE_URL}/api/v1/orders"
+    
