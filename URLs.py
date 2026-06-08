11bfffd109e580ffc041d19184d268edc46575fa
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-08 11:35:37 +0300

    Добавлены URL страницы и ручки для создания и авторизации курьера в системе в URLs.py

---
+++ URLs.py
@@ -0,0 +1,8 @@
+class URL:
+    BASE_URL = "https://qa-scooter.praktikum-services.ru"
+    
+    # Ручка - Создание курьера
+    CREATE_COURIER_ENDPOINT = f"{BASE_URL}/api/v1/courier"
+
+    # Ручка - Логин курьера в системе
+    LOGIN_COURIER_ENDPOINT = f"{BASE_URL}/api/v1/courier/login"
