ed8232154c352e196faf747d6bb1a6c94c72ff79
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-08 13:16:00 +0300

    Добавлен тест test_cannot_create_duplicate_courier в test_create_courier.py

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -16,3 +16,15 @@ class TestCreateCourier:
         assert response.status_code == 201, f"Ожидаемый статус код 201, но получили {response.status_code}"
         assert response.json()["ok"] is True
 
+
+    @allure.title('Нельзя создать двух одинаковых курьеров')
+    @allure.description("Тест проверяет, что нельзя создать двух курьеров с одинаковым логином")
+    def test_cannot_create_duplicate_courier(self):
+        courier_data = generate_new_courier_login_password_first_name()
+        response1 = CourierMethods.create_courier(courier_data)
+        response2 = CourierMethods.create_courier(courier_data)
+        response2_body = '{"message": "Этот логин уже используется"}'
+
+        assert response2.status_code == 409, f"Ожидаемый статус код 409, но получили {response2.status_code}" 
+        assert response2.json() == response2_body
+
