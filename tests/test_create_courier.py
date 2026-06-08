f677c92910d5a7e8712bd63ab75b9abab2ab0e17
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-06-08 13:17:30 +0300

    Добавлен тест test_cannot_create_courier_without_required_field в test_create_courier.py

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -28,3 +28,19 @@ class TestCreateCourier:
         assert response2.status_code == 409, f"Ожидаемый статус код 409, но получили {response2.status_code}" 
         assert response2.json() == response2_body
 
+
+    @pytest.mark.parametrize("key,value", [
+        ("login", ""),
+        ("password", ""),
+        ("firstName", "")
+        ])
+    @allure.title("Проверка, что все обязательные поля должны быть переданы")
+    @allure.description("Тест проверяет, что для создания нового курьера необходимо передать все обязательные поля")
+    def test_cannot_create_courier_without_required_field(self, key, value):
+        courier_data = generate_new_courier_login_password_first_name()
+        courier_data[key] = value
+
+        with allure.step(f"Попытка создания курьера с незаполненным полем: {key}"):
+            response = CourierMethods.create_courier(courier_data)
+        assert response.status_code == 400, f"Ожидаемый статус код 400, но получили {response.status_code}"
+        assert "Недостаточно данных для создания учетной записи" in response.json()["message"]
